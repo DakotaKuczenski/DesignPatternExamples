3c5154fc8383cd6e35949e5f5687534ad019e4e2
Dakota Kuczenski <dakotakuczenski@Dakotas-MBP.lan1>
2020-06-11 16:36:50 -0500

    o

--- ObserverPattern/Driver.java
+++ ObserverPattern/Driver.java
@@ -1,10 +1,14 @@
 
-public class Driver
-{
-    public static void main(String[] args)
-    {
-        Button button = new Button(); 
+public class Driver {
+    public static void main(String[] args) {
+       Subject subject = new Subject();
+ 
+       new AObserver(subject);
 
-        System.out.println("hello");
+ 
+       System.out.println("First state change: 15");	
+       subject.setState(15);
+       System.out.println("Second state change: 10");	
+       subject.setState(10);
     }
-}
+ }
